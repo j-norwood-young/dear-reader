f6431edbac4dbaebd38095883b6ad08c54746380
Jason Norwood-Young <jason@10layer.com>
2016-04-01 15:26:20 +0200

    config example

---
+++ config-example.js
@@ -0,0 +1,13 @@
+module.exports = {
+	port: 8080,
+	name: "dear-reader-server",
+	mongo: {
+		server: "localhost",
+		db: "dearreader"
+	},
+	randomChance: 0.1,
+	admin: {
+		username: "admin",
+		password: "password"
+	}
+};
